e6352873eed4857a9d9906ffbcd9708158d40543
Danila Mandrabura <danilamandrabura@gmail.com>
2024-10-29 14:34:44 +0200

    Fix smtp settings

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -6,7 +6,6 @@ export async function POST(request: NextRequest) {
   const transporter = nodemailer.createTransport({
     host: process.env.MAIL_HOST,
     port: process.env.MAIL_PORT,
-    secure: process.env.NODE_ENV !== "development",
     auth: {
       user: process.env.MAIL_USER,
       pass: process.env.MAIL_PASSWORD,
